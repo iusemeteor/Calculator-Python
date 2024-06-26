51547bdaa9a3bdb5b98f4eaca3910ea6fdbe5436
meteor <catboyshadow87@gmail.com>
2024-06-26 05:56:09 -0500

    Update calculator.py
    roar

--- calculator.py
+++ calculator.py
@@ -7,7 +7,6 @@ def adding():
     answer = str(answer)
     print("The answer is " + answer + ".")
 
-
 def subtract():
     first = input("What's the first number you would like to subtract? ")
     second = input("What's the first number you would like to subtract? ")
@@ -17,20 +16,18 @@ def subtract():
     answer = str(answer)
     print("The answer is " + answer + ".")
 
-
 def multiply():
-    first = input("What's the first number you would like to multiply?")
-    second = input("What's the first number you would like to multiply?")
+    first = input("What's the first number you would like to multiply? ")
+    second = input("What's the first number you would like to multiply? ")
     first = int(first)
     second = int(second)
     answer = first * second
     answer = str(answer)
     print("The answer is " + answer + ".")
 
-
 def division():
-    first = input("What's the first number you would like to divide?")
-    second = input("What's the first number you would like to divide?")
+    first = input("What's the first number you would like to divide? ")
+    second = input("What's the first number you would like to divide? ")
     first = int(first)
     second = int(second)
     answer = first / second
@@ -41,21 +38,21 @@ def main():
     print("Choose a mode:")
     print("1. Adding")
     print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
+    print("3. Multiply")
+    print("4. Divide")
     option = input("Enter option number: ")
-    option = int(option)
-    if option == 1:
-        adding()
-    elif option == 2:
-        subtract()
-    elif option == 3:
-        multiply()
-    elif option == 4:
-        division()
+    if option.isdigit():
+        option = int(option)
+        if option == 1:
+            adding()
+        elif option == 2:
+            subtract()
+        elif option == 3:
+            multiply()
+        elif option == 4:
+            division()
     else:
-        print("Invalid option number!")
-
+        print("Invalid option!")
 
 if __name__ == '__main__':
     main()
